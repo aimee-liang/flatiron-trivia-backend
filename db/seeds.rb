c2115442ffdc8e2652b9030c6f5665bd457aa48a
github username <github email address>
2020-10-20 12:07:24 -0400

    Seeds are working

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # require 'rest-client'
 # require 'json'
-require 'pry'
+# require 'pry'
 
 Question.destroy_all
 Category.destroy_all
@@ -21,13 +21,34 @@ flatiron_trivia = Game.create(user_id: user1.id, score: 0 )
 
 sport =Category.create(title: "Sports", game_id: flatiron_trivia.id)
 celebrity = Category.create(title: "Celebrities", game_id: flatiron_trivia.id)
-mythology = Category.create(title: "Mythology", game_id: flatiron_trivia.id)
+geography = Category.create(title: "Geography", game_id: flatiron_trivia.id)
 entertainment = Category.create(title: "Entertainment", game_id: flatiron_trivia.id)
 
-Question.create(category_id: sport.id, question: "Who was the British professional wrestler Shirley Crabtree better known as?", answer: "Big Daddy", incorrect_answers: ["Giant Haystacks", "Kendo Nagasaki", "Masambula"])
-Question.create(category_id: sport.id, question: "What team did England beat in the semi-final stage to win in the 1966 World Cup final?", answer: "Portugal", incorrect_answers: ["West Germany","Soviet Union","Brazil"])
-Question.create(category_id: sport.id, question: "In bowling, what is the term used for getting three consecutive strikes?", answer: "Turkey", incorrect_answers:  ["Flamingo","Birdie","Eagle"])
-Question.create(category_id: sport.id, question: "Which sport is NOT traditionally played during the Mongolian Naadam festival?", answer: "American Football", incorrect_answers: ["Wrestling", "Archery","Horse-Racing"])
+sport1 = Question.create(category_id: sport.id, question: "Who was the British professional wrestler Shirley Crabtree better known as?", answer: "Big Daddy", incorrect_answers: ["Giant Haystacks","Masambula", "Kendo Nagasaki"])
+# sport1.incorrect_answers.push("Giant Haystacks","Masambula", "Kendo Nagasaki")
+
+sport2 = Question.create(category_id: sport.id, question: "What team did England beat in the semi-final stage to win in the 1966 World Cup final?", answer: "Portugal", incorrect_answers: ["West Germany","Soviet Union","Brazil"])
+# sport2.incorrect_answers.push("West Germany","Soviet Union","Brazil")
+
+sport3 = Question.create(category_id: sport.id, question: "In bowling, what is the term used for getting three consecutive strikes?", answer: "Turkey", incorrect_answers: ["Flamingo","Birdie","Eagle"])
+# sport3.incorrect_answers.push("Flamingo","Birdie","Eagle")
+
+sport4 = Question.create(category_id: sport.id, question: "Which sport is NOT traditionally played during the Mongolian Naadam festival?", answer: "American Football", incorrect_answers: ["Wrestling", "Archery","Horse-Racing"])
+# sport4.incorrect_answers.push("Wrestling", "Archery","Horse-Racing")
+
+
+Question.create(category_id: entertainment.id, question: "", answer: "", incorrect_answers: [])
+Question.create(category_id: entertainment.id, question: "", answer: "", incorrect_answers: [])
+Question.create(category_id: entertainment.id, question: "", answer: "", incorrect_answers: [])
+Question.create(category_id: entertainment.id, question: "", answer: "", incorrect_answers: [])
+Question.create(category_id: entertainment.id, question: "", answer: "", incorrect_answers: [])
+
+Question.create(category_id: geography.id, question: "", answer: "", incorrect_answers: [])
+Question.create(category_id: geography.id, question: "", answer: "", incorrect_answers: [])
+Question.create(category_id: geography.id, question: "", answer: "", incorrect_answers: [])
+Question.create(category_id: geography.id, question: "", answer: "", incorrect_answers: [])
+Question.create(category_id: geography.id, question: "", answer: "", incorrect_answers: [])
+
 # sports_questions_response = RestClient.get("https://opentdb.com/api.php?amount=1&category=21&type=multiple")
 # sports_questions_data = JSON.parse(sports_questions_response)
 # puts sports_questions_data[:results][:questions]
